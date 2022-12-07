92503b6aec0f71b6d03d812763fc6d0adfea9a71
krisine <kristine.nyaga@student.moringaschool.com>
2022-12-08 01:37:03 +0300

    adds the header section and image section

---
+++ src/components/header/header.js
@@ -0,0 +1,18 @@
+import "./header.css"
+
+export default function Header(){
+  return(
+    <>
+     <div className="header">
+      <div className="headerTitle">
+        <span className="headerTitleSm">Scarlet</span>
+        <span className="headerTitleLg">N<span className="colour">e</span>ws</span>
+
+      </div>
+      
+    </div>
+    <h1 className="title">DAILY DOSE ..</h1>
+    </>
+   
+  );
+}

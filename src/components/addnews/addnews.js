95ca30557be6008e0c06ee95052418de09b31bf5
krisine <kristine.nyaga@student.moringaschool.com>
2022-12-08 01:37:48 +0300

    adds form where you can add to the news

---
+++ src/components/addnews/addnews.js
@@ -0,0 +1,70 @@
+import "./addnews.css"
+import { useState } from "react";
+export default function AddNews({user}){
+  const [category, setCategory] = useState("");
+  const [rating, setRating] = useState("");
+  const [content, setContent] = useState("");
+  const [image_url, setImage_Url] = useState("");
+  const [title, setTitle] = useState("");
+
+  function handleSubmit(){
+    console.log(rating);
+  }
+
+  return(
+    <div className="write">
+     <div className="main-block">
+      <h1>Add News</h1>
+      <form action="/" onSubmit={handleSubmit}>
+        <div className="info">
+          <input class="fname" type="text" name="name" placeholder="Full name" value={user.firstname}/>
+          <input type="text" className="email" placeholder="Email" value={user.email}/>
+          <input type="text" name="title" placeholder="Title"
+           value={title}
+           onChange={(e) => setTitle(e.target.value)}/>
+           
+           
+        <input type="text" name="image_url" placeholder="image_url"
+           value={image_url}
+           onChange={(e) => setImage_Url(e.target.value)}/>
+
+          <input type="text" name="category" placeholder="category"
+           value={category}
+           onChange={(e) => setCategory(e.target.value)}/>
+
+        <select
+           value={rating}
+           onChange={(e) => setRating(e.target.value)}>
+            {/* <option value="rating" disabled selected>Rating</option> */}
+            <option value="1">Rating</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+        </select>
+             
+          
+        </div>
+        <textarea
+            className="writeInput writeText"
+            placeholder="Tell your story..."
+            type="text"
+            autoFocus={true}
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        <div className="writeFormGroup">
+          
+        </div>
+        
+      
+      
+        <button  className="button">Submit</button>
+      </form>
+    </div>
+      </div>
+
+   
+  )
+}

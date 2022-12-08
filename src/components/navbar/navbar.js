75177ec23912a4ce0671ca4122d1e255c18ab5c0
krisine <kristine.nyaga@student.moringaschool.com>
2022-12-08 16:53:43 +0300

    adds logout functionality

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,8 +3,21 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {Link} from 'react-router-dom'
 import "./navbar.css"
+import { useNavigate } from "react-router-dom";
 
-function NavBar() {
+
+function NavBar({user,setUser}) {
+  const nav = useNavigate();
+
+    function handleLogoutClick() {
+      fetch("/logout", { method: "DELETE" }).then((r) => {
+        if (r.ok) {
+          setUser(null);
+          nav("/")
+        }
+      });
+    }
+
   return (
     <>
      
@@ -15,7 +28,9 @@ function NavBar() {
             <Nav.Link as={Link} to="/home">Home</Nav.Link>
             <Nav.Link as={Link} to="/write">Add News</Nav.Link>
             <Nav.Link as={Link} to="/editprofile">ChangeProfile</Nav.Link>
-          <Nav.Link as={Link} to="/logout">Logout</Nav.Link>
+            <button className='logoutBtn' onClick={handleLogoutClick}>
+          Logout
+        </button>
 
           </Nav>
        
